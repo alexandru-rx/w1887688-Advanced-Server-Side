8456b10ddb69bab9738f2e61f87fa216c3ccbbd9
alexandru-rx <165170964+alexandru-rx@users.noreply.github.com>
2026-04-01 20:42:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.use(
   cors({
     origin: [
       "http://127.0.0.1:5500",
-      "http://localhost:5500"
+      "http://localhost:5500",
+      "https://alexandru-rx.github.io"
     ],
     credentials: true
   })
